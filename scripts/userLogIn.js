ee3fd867faa0f4455015bb6fa627a08b51aeb4d2
Gamerkingqw <sanad.suhail12@gmail.com>
2025-01-22 00:00:53 +0100

    added stuff and first javascript

---
+++ scripts/userLogIn.js
@@ -0,0 +1,26 @@
+console.log(document.cookie);
+console.log(2*3);
+//const userSearchbar = document.getElementsByClassName('userSearchbar');
+//const passwordSearchbar = document.getElementsByClassName('passwordSearchbar');
+//alert("alert");
+//confirm("confirm");
+
+/** var user = prompt('user');
+var password = prompt('password');
+
+var userData = [,];
+userData[0,0] = user
+userData[0,1] = password
+
+console.log(userData[0,1]);
+console.log(password[0,1]);
+*/
+
+
+
+/** setTimeout(function(){
+    console.log(wert);
+}, 5000);
+*/
+
+
